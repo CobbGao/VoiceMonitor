ce13c7f90536f6502b0e3a1e96c16af4e46ad362
gaobowen <gaobowen@xiaomi.com>
2023-07-04 13:36:10 +0800

    support clipboard algo
    Signed-off-by: gaobowen <gaobowen@xiaomi.com>

--- src/jvmMain/kotlin/GptHelper.kt
+++ src/jvmMain/kotlin/GptHelper.kt
@@ -1,5 +1,6 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import org.json.JSONObject
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.StandardCharsets
@@ -7,9 +8,10 @@ import java.nio.charset.StandardCharsets
 object GptHelper {
     private const val ADDRESS = "https://api.texttools.cn/api/chat/stream"
 
-    var PREFIX = "假设场景：你是一个Java程序员，正在参加程序员的面试，现在我向你提出问题，请尝试从一个程序员的角度回答问题。" +
-            "由于我提出的问题文本来自于语音识别技术，当问题晦涩难懂时，尝试根据中文发音猜测对应英文词汇来理解问题，一些可能对应的英文单词有：Java、Spring Boot。" +
+    var PREFIX_VOICE = "假设场景：你是一个Java程序员，正在参加程序员的面试，现在我向你提出问题，请尝试从一个程序员的角度回答问题。" +
+            "问题的文本内容来自于语音识别技术，当问题晦涩难懂时，尝试根据中文发音猜测对应英文词汇来理解问题，一些可能出现的英文单词有：Java、Spring Boot。" +
             "下面是问题："
+    var PREFIX_ALGO = "解答下列问题，给出Java实现。优先给出性能更好的实现。尽量避免使用递归思想。先给出代码实现，再阐述解题思路。"
 
     val messageFlow = MutableStateFlow("")
 
@@ -19,14 +21,25 @@ object GptHelper {
         currentJob?.cancel()
         currentJob = ApplicationDefaultScope.launch(Dispatchers.IO) {
             if (content.isBlank()) {
-                messageFlow.value = "prefix: $PREFIX\nquery: [无语音识别结果]:\n"
+                messageFlow.value = "query: [无语音识别结果]:\n"
                 return@launch
             }
-            forwardInner(this, content)
+            forwardInner(this, PREFIX_VOICE, content)
         }
     }
 
-    private fun forwardInner(scope: CoroutineScope, content: String) {
+    fun algo(content: String) {
+        currentJob?.cancel()
+        currentJob = ApplicationDefaultScope.launch(Dispatchers.IO) {
+            if (content.isBlank()) {
+                messageFlow.value = "query: [剪贴板无文本内容]:\n"
+                return@launch
+            }
+            forwardInner(this, PREFIX_ALGO, content)
+        }
+    }
+
+    private fun forwardInner(scope: CoroutineScope, prefix: String, content: String) {
         messageFlow.value = "content: $content:\n"
         val url = URL(ADDRESS)
         val connection = url.openConnection() as HttpURLConnection
@@ -34,10 +47,10 @@ object GptHelper {
         connection.setRequestProperty("Content-Type", "application/json")
         connection.setRequestProperty("apikey", "textAxVT0iq3uFfmDmDHr2n7cHeISPXmPljykOBL5VbPxeJv")
         connection.setRequestProperty("Accept", "text/event-stream")
-        val requestBody = "{\"content\": \"$PREFIX$content\"}"
+        val requestBody = JSONObject().apply { put("content", "$prefix$content") }
         connection.doOutput = true
         connection.outputStream.use {
-            it.write(requestBody.toByteArray())
+            it.write(requestBody.toString().toByteArray())
             it.flush()
         }
         val responseCode = connection.responseCode
@@ -50,7 +63,7 @@ object GptHelper {
         var len: Int
         val inputStream = connection.inputStream
         while (inputStream.read(bytes).also { len = it } != -1 && scope.isActive) {
-            messageFlow.value += String(bytes, 0, len, StandardCharsets.UTF_8)
+            messageFlow.value += String(bytes, 0, len, StandardCharsets.UTF_8).replace("\n\n", "\n")
         }
         connection.disconnect()
     }
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,24 +1,42 @@
-import GptHelper.PREFIX
+import GptHelper.PREFIX_ALGO
+import GptHelper.PREFIX_VOICE
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.github.kwhat.jnativehook.GlobalScreen
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent
 import com.github.kwhat.jnativehook.mouse.NativeMouseListener
+import com.github.kwhat.jnativehook.mouse.NativeMouseWheelEvent
+import com.github.kwhat.jnativehook.mouse.NativeMouseWheelListener
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.awt.Robot
+import java.awt.Toolkit
+import java.awt.datatransfer.DataFlavor
+import java.awt.event.KeyEvent
 import java.util.LinkedList
 import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioFormat.Encoding.PCM_SIGNED
 import javax.sound.sampled.AudioSystem
 
-const val CONFIG_BUTTON_CODE = 4
+private const val BUTTON_CODE_LEFT = 1
+
+const val BUTTON_CODE_VOICE = 4
+const val BUTTON_CODE_CLIPBOARD = 5
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, alwaysOnTop = true) {
@@ -29,36 +47,73 @@ fun main() = application {
 @Composable
 @Preview
 fun App() {
-    startMonitor()
+    val scrollState = rememberScrollState()
+    startMonitor(scrollState)
 
     MaterialTheme {
         Column {
-            var text by remember { mutableStateOf(PREFIX) }
+            var text1 by remember { mutableStateOf(PREFIX_VOICE) }
             TextField(
-                value = text,
-                onValueChange = { PREFIX = it; text = it },
+                modifier = Modifier.fillMaxWidth(),
+                value = text1,
+                onValueChange = { PREFIX_VOICE = it; text1 = it },
+                textStyle = TextStyle(fontSize = 10.sp),
+                singleLine = true,
+            )
+            var text2 by remember { mutableStateOf(PREFIX_ALGO) }
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = text2,
+                onValueChange = { PREFIX_ALGO = it; text2 = it },
+                textStyle = TextStyle(fontSize = 10.sp),
+                singleLine = true,
             )
             val response by GptHelper.messageFlow.collectAsState()
-            Text(text = response)
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(scrollState),
+                text = response,
+                fontSize = 10.sp,
+            )
         }
     }
 }
 
-private fun startMonitor() {
+private fun startMonitor(scrollState: ScrollState) {
     GlobalScreen.registerNativeHook()
+
+    adaptScroll(scrollState)
+
+    startVoiceMonitor()
+    startClipboardMonitor()
+}
+
+fun adaptScroll(scrollState: ScrollState) {
+    GlobalScreen.addNativeMouseWheelListener(object : NativeMouseWheelListener {
+        override fun nativeMouseWheelMoved(nativeEvent: NativeMouseWheelEvent?) {
+            val direction = nativeEvent?.wheelRotation ?: return
+            ApplicationDefaultScope.launch {
+                scrollState.scrollBy(direction * 5f)
+            }
+        }
+    })
+}
+
+private fun startVoiceMonitor() {
     GlobalScreen.addNativeMouseListener(object : NativeMouseListener {
         private var start: Boolean = false
         private var job: Job? = null
 
         override fun nativeMousePressed(nativeEvent: NativeMouseEvent?) {
-            if (nativeEvent?.button != CONFIG_BUTTON_CODE) {
+            if (nativeEvent?.button != BUTTON_CODE_VOICE) {
                 return
             }
             startCapture()
         }
 
         override fun nativeMouseReleased(nativeEvent: NativeMouseEvent?) {
-            if (nativeEvent?.button != CONFIG_BUTTON_CODE) {
+            if (nativeEvent?.button != BUTTON_CODE_VOICE) {
                 return
             }
             endCapture()
@@ -66,7 +121,7 @@ private fun startMonitor() {
 
         private fun startCapture() {
             start = true
-            println("find mouse button [$CONFIG_BUTTON_CODE] pressed, start to capture system audio output stream...")
+            println("find mouse button [$BUTTON_CODE_VOICE] pressed, start to capture system audio output stream...")
             job?.cancel()
             job = ApplicationDefaultScope.launch(Dispatchers.IO) {
                 val audioFormat = AudioFormat(PCM_SIGNED, 16000f, 16, 1, (16 / 8) * 1, 16000f, false)
@@ -88,7 +143,42 @@ private fun startMonitor() {
 
         private fun endCapture() {
             start = false
-            println("mouse button [$CONFIG_BUTTON_CODE] released, stop capture.")
+            println("mouse button [$BUTTON_CODE_VOICE] released, stop capture.")
+        }
+    })
+}
+
+private fun startClipboardMonitor() {
+    GlobalScreen.addNativeMouseListener(object : NativeMouseListener {
+        private val robot = Robot()
+        private var time = 0L
+        override fun nativeMousePressed(nativeEvent: NativeMouseEvent?) {
+            when(nativeEvent?.button) {
+                BUTTON_CODE_LEFT -> { time = System.currentTimeMillis() }
+            }
+        }
+
+        override fun nativeMouseReleased(nativeEvent: NativeMouseEvent?) {
+            when(nativeEvent?.button) {
+                BUTTON_CODE_LEFT -> {
+                    if (System.currentTimeMillis() - time > 500L) {
+                        robot.keyPress(KeyEvent.VK_CONTROL)
+                        robot.keyPress(KeyEvent.VK_C)
+                        robot.keyRelease(KeyEvent.VK_C)
+                        robot.keyRelease(KeyEvent.VK_CONTROL)
+                    }
+                }
+            }
+        }
+
+        override fun nativeMouseClicked(nativeEvent: NativeMouseEvent?) {
+            if (nativeEvent?.button != BUTTON_CODE_CLIPBOARD) {
+                return
+            }
+            println("mouse button [$BUTTON_CODE_CLIPBOARD] clicked, query algo.")
+            with(Toolkit.getDefaultToolkit().systemClipboard) {
+                GptHelper.algo(getContents(null).getTransferData(DataFlavor.stringFlavor) as String)
+            }
         }
     })
 }
